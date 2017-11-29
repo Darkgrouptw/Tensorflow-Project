59941122ff626446aef0949c681954d7f2a977f3
Dark <darkgrouptw@gmail.com>
2017-11-29 19:32:13 +0800

    測試 tensorboard

--- 05. Policy Gradient in Breakout-v0/RLAgent_PolicyGradient_CNN.py
+++ 05. Policy Gradient in Breakout-v0/RLAgent_PolicyGradient_CNN.py
@@ -1,5 +1,9 @@
 import numpy as np
 import tensorflow as tf
+
+np.random.seed(3)
+tf.set_random_seed(3)
+
 class PolicyGradient:
     def __init__(
             self,
@@ -41,12 +45,12 @@ class PolicyGradient:
     def BuildNet(self):
         # 參數設定
         # 用的 filter 數目及大小
-        filter1Count = 16
-        filter1Size = 5
-        filter2Count = 20
-        filter2Size = 5
-        fc1Count = 128
-        fc2Count = 64
+        filter1_Count = 16
+        filter1_Size = 5
+        filter2_Count = 20
+        filter2_Size = 5
+        fc1_Count = 128
+        fc2_Count = 64
 
         # Input
         with tf.name_scope("Input"):
@@ -59,12 +63,14 @@ class PolicyGradient:
         # 亂數
         weightInit = tf.random_normal_initializer(mean=0, stddev = 0.3)
         biasInit = tf.random_normal_initializer(mean=0, stddev = 0.1)
-
+        
+        # Conv2d 會做一件事，把 Input 攤平 [filter_height * filter_width * in_channels, output_channels].
+        # 所以如果要處理多個 Channel 的資訊，例如三個顏色，可能需要用到 conv3d
         # Conv1
         conv1 = tf.layers.conv2d(
             inputs = self.observationsArray,
-            filters = filter1Count,
-            kernel_size = filter1Size,
+            filters = filter1_Count,
+            kernel_size = filter1_Size,
             strides = 1,
             padding = "same",
             activation = tf.nn.relu,
@@ -76,8 +82,8 @@ class PolicyGradient:
         # Conv2
         conv2 = tf.layers.conv2d(
             inputs = conv1,
-            filters = filter2Count,
-            kernel_size = filter2Size,
+            filters = filter2_Count,
+            kernel_size = filter2_Size,
             strides = 1,
             padding = "same",
             activation = tf.nn.relu,
@@ -87,12 +93,12 @@ class PolicyGradient:
         )
 
         # 把資料壓平
-        flatData = tf.reshape(conv2, [-1])
+        flatData = tf.reshape(conv2, [-1, 105 * 80 * 20])
 
         # Fully Connected 1
         fc1 = tf.layers.dense(
             inputs = flatData,
-            units = fc1Count,
+            units = fc1_Count,
             activation = tf.nn.relu,
             kernel_initializer = weightInit,
             bias_initializer = biasInit,
@@ -101,24 +107,24 @@ class PolicyGradient:
 
         fc2 = tf.layers.dense(
             inputs = fc1,
-            units = fc2Count,
+            units = fc2_Count,
             activation = tf.nn.relu,
             kernel_initializer = weightInit,
             bias_initializer = biasInit,
             name = "FC2"
         )
 
-        fc3 = tf.layers.dense(
+        output = tf.layers.dense(
             inputs = fc2,
             units = self.n_actions,
             kernel_initializer=weightInit,
             bias_initializer=biasInit,
-            name = "FC3"
+            name = "output"
         )
 
 
         # 每個動作的機率
-        self.ActionProb = tf.nn.softmax(fc3, name="ActionProb")
+        self.ActionProb = tf.nn.softmax(output, name="ActionProb")
 
         # Loss Function
         with tf.name_scope("Loss"):
@@ -127,7 +133,7 @@ class PolicyGradient:
             # cost 跟 cos1 算出來的結果不一樣，一個是已經 reduce_mean 完的結果
             # cost1 = tf.losses.softmax_cross_entropy(logits=layer2, onehot_labels=actionNum_OneHot)
             # cost = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=layer2, labels=self.actionsNum)
-            cost = tf.nn.softmax_cross_entropy_with_logits(logits=layer2, labels=actionNum_OneHot)
+            cost = tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=actionNum_OneHot)
 
             # 這裡的意思是，只有剛開始不會倒的情況下， DeltaValue 就會越大，代表他這部分要越重視!!
             # 下面那個註解掉的
@@ -143,9 +149,10 @@ class PolicyGradient:
 
     # 根據觀察的結果，選擇動作
     def chooseAction(self, observation):
-        InputData = {self.observations: [observation]}
+        InputData = {self.observationsArray: [observation]}
         actionProb = self.session.run(self.ActionProb, feed_dict= InputData)
-
+        #print(actionProb)
+        
         # 因為確保動作是 Int(0 => 左，1 => 右)
         # 所以不給 size (所以只會回傳出一個 Int)
         # 如果給其他的 size 會回傳出一個陣列，連 1 也是個陣列 ex: [2]
@@ -162,11 +169,12 @@ class PolicyGradient:
     # 開始學習
     def learn(self):
         normalizedReward = self.MakeReward()
+        print(normalizedReward)
 
         self.session.run(
             self.optimizer,
             feed_dict={
-                self.observations: self.MemoryObservation,
+                self.observationsArray: self.MemoryObservation,
                 self.actionsNum: self.MemoryAction,
                 self.DeltaValue: normalizedReward
             })
@@ -190,24 +198,24 @@ class PolicyGradient:
         # reward，最一開始是0
         reward = 0
 
-        # 立竿子的遊戲
-        # 是前面的 reward 比較大
-        # 後面的reward 比較小
-        # 稱越久
-        # 代表最一開始的動作很有價值
+        # 在 Breakout 的遊戲中
+        # 希望讓他打磚塊的前面幾個 reward 比較高
+        # 其他的 reward 比較小
+        # 目的是要讓他越打到越好
         # 要從 len(normalizeReward) - 1 到 0，但是range 不會包含到最後面，所以才要 -1
         for i in range(len(normalizeReward) - 1, -1, -1):
             reward = reward * self.RewardDecay + self.MemoryReward[i]
             normalizeReward[i] = reward
-        # for t in reversed(range(0, len(self.MemoryReward))):
-        #     reward = reward * self.RewardDecay + self.MemoryReward[t]
-        #     normalizeReward[t] = reward
 
         # 這邊在做的事情是
         # 為了要讓 reward 平均
         # 不會太大或太小 (因為 array 可能會很大，所以要讓後面的東西變小)
         # 及 拉近差距 (/ dev)
         normalizeReward -= np.mean(normalizeReward)
-        normalizeReward /= np.std(normalizeReward)
+        
+        # 讓資料間的距離縮短
+        std = np.std(normalizeReward)
+        if(std != 0):
+            normalizeReward /= std
         return normalizeReward
 
